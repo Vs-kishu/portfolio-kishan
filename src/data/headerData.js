3042b3e88b8d319cbf013e241e142c767a41031b
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2022-12-09 14:57:51 +0530

    second

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,8 +4,8 @@ export const headerData = {
   desciption:
     "Hello! I am KISHAN, I have good Programming skills and I am a great team-player. I love to work in groups. I have great observation skills and have a really optimistic approach towards my work. I am always eager to learn new things. I would definitely leave no stone unturned to prove as an asset for your company.I will surely not disappoint you with my work.",
   image:
-    "https://scontent.flko7-1.fna.fbcdn.net/v/t39.30808-6/302420973_1405136889973156_3276970877869430920_n.jpg?stp=dst-jpg_s960x960&_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=wUSO-w2RYIIAX_tylIx&_nc_oc=AQlQV5cTYQojNYVrK7f2ELPEiuVg7Xy-BOQh5iYb6Sq_h_2bBDSZZeLfnNNtpZhiWRw&_nc_ht=scontent.flko7-1.fna&oh=00_AfBJJEaPjSQzqekG7gf1oSQP1XEVUAw7-fagK-tETibc_g&oe=6390274E",
+    "https://scontent.flko7-2.fna.fbcdn.net/v/t39.30808-6/273687522_1273914289762084_3736985728793535586_n.jpg?stp=dst-jpg_p843x403&_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=74-ZjYkiFgcAX_06LNa&_nc_ht=scontent.flko7-2.fna&oh=00_AfD2pGC1KqjkBHXHPkkRMkCCQaVu0o0xMLiJ5ciLGEONaw&oe=63978C8E",
   imagebw:
-    "https://scontent.flko7-1.fna.fbcdn.net/v/t39.30808-6/302420973_1405136889973156_3276970877869430920_n.jpg?stp=dst-jpg_s960x960&_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=wUSO-w2RYIIAX_tylIx&_nc_oc=AQlQV5cTYQojNYVrK7f2ELPEiuVg7Xy-BOQh5iYb6Sq_h_2bBDSZZeLfnNNtpZhiWRw&_nc_ht=scontent.flko7-1.fna&oh=00_AfBJJEaPjSQzqekG7gf1oSQP1XEVUAw7-fagK-tETibc_g&oe=6390274E",
+    "https://scontent.flko7-2.fna.fbcdn.net/v/t39.30808-6/273687522_1273914289762084_3736985728793535586_n.jpg?stp=dst-jpg_p843x403&_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=74-ZjYkiFgcAX_06LNa&_nc_ht=scontent.flko7-2.fna&oh=00_AfD2pGC1KqjkBHXHPkkRMkCCQaVu0o0xMLiJ5ciLGEONaw&oe=63978C8E",
   resumePdf: "",
 };
