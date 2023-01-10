00a279804a5a6ee15f1393b5aaf53bee781766a2
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2023-01-10 11:24:01 +0530

    changed style

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,24 +1,24 @@
 /* eslint-disable */
-import { contactsGreen } from './images'
+import { contactsGreen } from "./images";
 
 export const theLightTheme = {
-    type: 'light',
-    primary: '#1D9BF0',
-    quaternary: '#EFF3F4',
-    quaternaryLight: '#F7F9F9',
-    secondary: '#ffffff',
-    tertiary: '#0f1419',
-    buttonColor: '#0F1419',
-    contactsimg: contactsGreen
-}
+  type: "light",
+  primary: "#7E3517",
+  quaternary: "#EFF3F4",
+  quaternaryLight: "#F7F9F9",
+  secondary: "#ffffff",
+  tertiary: "#0f1419",
+  buttonColor: "#0F1419",
+  contactsimg: contactsGreen,
+};
 
 export const theDarkTheme = {
-    type: 'dark',
-    primary: '#1D9BF0',
-    quaternary: '#1E2732',
-    quaternaryLight: '#8b98a5',
-    secondary: '#15202B',
-    tertiary: '#EFF3F4',
-    buttonColor: '#8B98A5',
-    contactsimg: contactsGreen
-}
+  type: "dark",
+  primary: "#B21807",
+  quaternary: "#0C090A",
+  quaternaryLight: "#FEFCFF",
+  secondary: "#3B2F2F",
+  tertiary: "#EFF3F4",
+  buttonColor: "#F9966B",
+  contactsimg: contactsGreen,
+};
